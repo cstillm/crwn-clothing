4d2b9e5572eeb86ea36ace3f0f4109ee8fb6be49
Christopher Stillman <christopher.stillman@gmail.com>
2020-05-07 16:05:02 -0700

    updated with collection preview and item compents

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage}></Route>
-        <Route></Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );

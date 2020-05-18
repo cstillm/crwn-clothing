f53795aa893685112b33de8b32a2935c9a6a4f5c
Christopher Stillman <christopher.stillman@gmail.com>
2020-05-17 22:16:56 -0700

    authenticate users and store users in firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component  {
   constructor() {
@@ -20,10 +20,30 @@ class App extends React.Component  {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+   
+      // check to see if a user is sign in, if they are we get back a userRef object from the
+      // userAuth object if there is no object we will create one in firebase.utils
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        // here we subscribe / listen to any changes to the userRef object, and will get back
+        // the first state of that data 'snapShot' and with that we will set the local state
+        // of our local app js, with the snapShot.id and ...snapShot.data()
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {console.log(this.state)});
+
+        });
+      } else {
+        // if the user ever logs out we will set the currenUser to userAuth which is the null
+        // we get back from the auth library
+        this.setState({ currentUser: userAuth })
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,9 +11,38 @@ const config = {
     messagingSenderId: "1068175258667",
     appId: "1:1068175258667:web:2526ce6406e5f4fcf5266e",
     measurementId: "G-7VYEFDJKW3"
-  };
+};
 
+// firebase setup 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // check if user is authenticated
+  if (!userAuth) return;
+
+  // if user auth then check if user exists in firebase
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  // if user uid doesn't exist we want to create user on firestore db
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error create user', error.message)
+    }
+  }
+
+  return userRef;
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
